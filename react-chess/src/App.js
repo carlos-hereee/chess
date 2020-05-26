e4fd854f739f3352bdb8b792a662a26e8d960588
ch04937 <97hernandez.c@gmail.com>
2020-05-26 15:48:14 -0500

    feat: boiler plate for header and footer

--- react-chess/src/App.js
+++ react-chess/src/App.js
@@ -3,16 +3,19 @@ import { Route } from "react-router-dom";
 
 import Game from "./pages/game";
 import Home from "./pages/home";
+import Header from "./pages/header";
+import Profile from "./pages/profile";
+import Fotter from "./pages/footer";
 
 import PrivateRoute from "./utils/PrivateRoute";
 import { ChessState } from "./utils/context/Chess/ChessState";
 
 import "./App.css";
-import Profile from "./pages/profile";
 
 function App() {
 	return (
 		<div className="App">
+			<Header />
 			<h1>Chess App</h1>
 			<Route exact path="/" component={Home} />
 			<Route path="/user" component={Profile} />
@@ -21,6 +24,7 @@ function App() {
 					<Game />
 				</ChessState>
 			</PrivateRoute>
+			<Fotter />
 		</div>
 	);
 }
